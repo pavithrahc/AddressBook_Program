2011f7438cd2bbf9e86060ded3ffe286e949228d
pavithramuddu93 <pavithramuddu93>
2022-09-13 21:20:22 +0530

    Welcome

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class AddressBookMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Welcome to Address Book ");
+
+	}
+
+}
