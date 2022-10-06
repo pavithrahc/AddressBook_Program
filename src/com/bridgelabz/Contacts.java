b7485d854d0776e23344fbc45793682dccf31c42
pavithramuddu93 <pavithramuddu93>
2022-10-06 07:51:44 +0530

    Added UC7 :No-Duplicate-Contact

---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class Contacts {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
